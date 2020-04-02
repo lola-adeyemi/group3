ed3844ed167c187ad53e58228557599383b34b92
Lola <lola.adeyemi91@googlemail.com>
2020-04-03 00:34:07 +0100

    gym php

---
+++ Gymphp/discounts.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Discounts</title>
+        <style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+}
+td {
+  text-align: center;
+}
+       </style>
+   </head>
+   
+    <body>
+        <h2> Discounts available in your gym </h2>
+        <br>
+        <p>Find out here the best discounts and choose the best option.</p>
+        <br>
+        <table style="width:60%">
+  <tr>
+    <th>Name</th>
+    <th>Percentage(%)</th> 
+    <th>Expiry months</th>
+  </tr>
+  <tr>
+    <td>Student</td>
+    <td>30</td>
+    <td>6</td>
+  </tr>
+        </table>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
